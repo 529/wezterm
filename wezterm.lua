b50e1a5b58ef9aaa07bba49dc48b48944d9b03ae
529 <koji.sakata@gmail.com>
2024-09-27 18:09:00 +0900

    20240927 mod

--- wezterm.lua
+++ wezterm.lua
@@ -3,6 +3,10 @@ local mux = wezterm.mux
 
 config = {}
 
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
 config.font_dirs = {'~/Library/Fonts/'}
 config.font = wezterm.font_with_fallback({
     {family='Source Han Code JP', weight="Regular", stretch='Normal'},
@@ -11,11 +15,14 @@ config.font = wezterm.font_with_fallback({
     'DengXian',
 })
 config.use_ime = true
+config.macos_forward_to_ime_modifier_mask = 'SHIFT|CTRL'
 config.font_size = 18.0
 config.color_scheme = "nord"
+config.window_background_opacity = 0.93
 config.hide_tab_bar_if_only_one_tab = true
 config.adjust_window_size_when_changing_font_size = fals
 
+--[[
 wezterm.on('gui-startup', function(cmd)
     local tab, top_pane, window = mux.spawn_window(cmd or {})
     print("mux.spawn_window: ", tab, top_pane, window)
@@ -25,5 +32,12 @@ wezterm.on('gui-startup', function(cmd)
     top_pane:activate()
     window:spawn_tab { }
 end)
+]]--
+
+config.disable_default_key_bindings = true
+config.leader = { key = 's', mods = 'CTRL', timeout_milliseconds = 2000 }
+local keybind = require 'keybinds'
+config.keys = keybind.keys
+config.key_tables = keybind.key_tables
 
 return config
